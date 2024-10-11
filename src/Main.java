f09e225f151e5810d55f578b83e0cae3b4c6a0ee
EdgarFunes <edgardoibarfunesortiz@gmail.com>
2024-10-11 18:02:02 -0300

    ejercicio 1

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,8 +10,9 @@ public class Main {
                         null,
                         """
                                 ¿Que ejercicio desea ejecutar?
+                                0. Salid
                                 1. Calcular el precio final con descuentos
-                                2.
+                                2. 
                                 3.
                                 4.
                                 5.
@@ -22,7 +24,9 @@ public class Main {
                 switch(option){
                     case 0:
                         JOptionPane.showMessageDialog(null, "Programa finalizado");
+                        break;
                     case 1:
+                        ejercicio1();
                         break;
                     default:
                         JOptionPane.showMessageDialog(null, "Opcion no valida");
@@ -31,5 +35,32 @@ public class Main {
                 JOptionPane.showMessageDialog(null, "Error: "+e.getMessage());
             }
         }while(option != 0);
+    }
+//-----------------------Ejercicios-------------------------
+    public static void ejercicio1(){
+        int[] ventas = new int[30];
+        Random random = new Random();
+        StringBuilder mensaje = new StringBuilder("Ventas diarias(generacion random)\n");
+        int ward = 0;
+
+        for (int i = 0; i < ventas.length; i++) {
+            ventas[i] = random.nextInt(1000);
+            mensaje.append(ventas[i]+", ");
+            if(ward == 6) {
+                mensaje.append("\n");
+                ward = 0;
+            }else ward++;
+        }
+        mensaje.append("\nVentas totales: ").append(calcularIngresosMensuales(ventas));
+            JOptionPane.showMessageDialog(null, mensaje);
+    }
+
+//----------------------Funciones--------------------------
+    public static int calcularIngresosMensuales(int[] ventas){
+        int suma = 0;
+        for (int venta : ventas) {
+            suma += venta;
+        }
+        return suma;
     }
 }
